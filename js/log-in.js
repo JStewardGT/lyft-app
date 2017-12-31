4011633ad069a65e9d1038b6659909f93291dc10
Yelitza3125 <deyel_99@hotmail.com>
2017-12-30 19:46:18 -0500

    Estableciendo función para direccionar a la vista de registro

---
+++ js/log-in.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  /* Funcion para redireccionar la vista al formulario */    
+  $('.btn-sign').click(function() {
+    window.location.href = '../views/register.html';   
+  }); 
+});
+  
