59c67444adb2bbf92264106121e4796b68112f4f
Yelitza3125 <deyel_99@hotmail.com>
2017-12-31 01:53:41 -0500

    Creando la funcionalidad de verificación del input y la alerta

--- js/register.js
+++ js/register.js
@@ -1,5 +1,43 @@
 $(document).ready(function() {
-    
-  /* Funcion para animar el logo */
 
+  /* Función que comprueba cuando el usuario sale del input */
+  $('#input-phone').keyup(function() {
+    var form = $(this).parents('#form');
+    /* Guardando en una función la variable de comprobar el formulario*/
+    var check = checkCampos(form);
+    if (check) {
+      /* Habilitando el botón */ 
+      $('.next').prop('disabled', false);
+    } else {
+      $('.next').prop('disabled', true);
+    }
+  });
+
+
+  /* Función para comprobar si el input está llenado correctamente */
+  function checkCampos(obj) {
+    /* Se inicializa con el input vacío */  
+    var inputFull = false;
+    /* Va a buscar que cada input este lleno */
+    obj.find('input').each(function() {
+      var $this = $(this);
+      /* Condicionando que la longitud del valor del input debe ser 10 */
+      if ($this.val().length === 10) {
+        inputFull = true;
+        return true;
+      }
+    });
+
+    if (inputFull === false) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  /* Función de envío del alerta con el código*/
+  $('.next').click(function() {
+    var number = Math.round(Math.random() * 999);
+    alert('LAB-' + number);
+  }); 
 });
