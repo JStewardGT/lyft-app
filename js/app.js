0e4d7cc534097198d5a9171de791887dd08033da
Yelitza3125 <deyel_99@hotmail.com>
2017-12-30 19:10:56 -0500

    Generación de la vista splash

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  /* Funcion para animar el logo */
+  setInterval(function logo() {
+    $('.logo').animate({width: '20%'}, 'slow');
+    $('.logo').animate({width: '50%'}, 'siwng');
+ }, 3000);
+    
+  setTimeout(function() {
+    window.location.href = 'views/log-in.html';
+  }, 5000); 
+
+
+});
