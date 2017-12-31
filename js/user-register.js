7b2e702b7c2f249d93061eca67f6ab2bf91111dc
Yelitza3125 <deyel_99@hotmail.com>
2017-12-31 18:47:10 -0500

    Redireccionando la página a la vista final

--- js/user-register.js
+++ js/user-register.js
@@ -12,56 +12,67 @@ $(document).ready(function() {
     }
   });
 
-
   /* Función para comprobar si todos los input están llenados correctamente e ir habilitándolos uno por uno */
-  function checkCampos() {
+  function checkCampos(obj) {
     /* Llamando a las variables */
     var inputFull = false;
+
     var $inputName = $('#user-name');
     var $inputLastName = $('#user-last-name');
     var $email = $('#inputEmail3');
     var $checkbox = $('#checkbox');
 
-    /* Validando el input del nombre */
-    if ($inputName.val() !== '' && $inputName.val().length >= 3) {
-      /* Si la condición se cumple, se habilita el siguiente input y se le aplica focus */
-      $inputLastName.prop('disabled', false);
-      $inputLastName.addClass('focus-input');
-      /* Si no se cumple el siguiente input sigue desabilitado */
-    } else {
-      $inputLastName.prop('disabled', true);
-    } if ($inputLastName.val() !== '' && $inputLastName.val().length >= 3) {
-      /* Si la condición se cumple, se habilita el siguiente input y se le aplica focus */
-      $email.prop('disabled', false);
-      $email.addClass('focus-input');
-      $('#checkbox').prop('disabled', false);
-
-      /* Función de verificación de email */
-      function verifyEmail($email) {
-        // console.log(email);
-        // Guardando el valor del input del email.
-        var email = $(email).val();
-        /* Usando una expresión regular para condicionar los caracteres que se ingresan en el input del email  */
-        var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
-        /* Validando los caracteres ingresados en el input del email */
-        if (caract.test(email) === false) {
-          email.prop('disabled', false);             
-        } else {
-          email.prop('disabled', true);
-        }
+    /* Va a buscar que cada input este lleno */
+    obj.find('input').each(function() {
+      /* Condicionando que la longitud del valor del input debe ser 10 */
+      if ($inputName.val() !== '' && $inputName.val().length >= 3) {
+        /* Si la condición se cumple, se habilita el siguiente input y se le aplica focus */
+        $inputLastName.prop('disabled', false);
+        $inputLastName.addClass('focus-input');
+        inputFull = true;
+      
+        /* Si no se cumple el siguiente input sigue desabilitado */
+      } else {
+        inputFull === false;
       }
-    } else {
-      $email.prop('disabled', true);
-    }
+      
+      if ($inputLastName.val() !== '' && $inputLastName.val().length >= 3) {
+        /* Si la condición se cumple, se habilita el siguiente input y se le aplica focus */
+        $email.prop('disabled', false);
+        $email.addClass('focus-input');
+        verifyEmail();
+      } else {
+        inputFull === false;
+      }
+    });
+
+    /* Función de verificación de email */
+    function verifyEmail($email) {
+      // console.log(email);
+      // Guardando el valor del input del email.
+      var email = $(email).val();
+      /* Usando una expresión regular para condicionar los caracteres que se ingresan en el input del email  */
+      var caract = new RegExp(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/);
+      /* Validando los caracteres ingresados en el input del email y habilitando el checkbox*/
+      if (caract.test(email) === false) { 
+        $checkbox.prop('disabled', false);            
+      }        
+    };
+
     /* Función de comprobación del checkbox */
-    $($checkbox).click(function() {
-      if ($checkbox.is(':checked') === true) {
+    $checkbox.click(function() {
+      if ($checkbox.is(':checked') === true) {  
         $('.next').prop('disabled', false);
+        
       } else {
         alert('Debe aceptar las condiciones');
         event.preventDefault();
-        $('.next').prop('disabled', true);
       }
-    });    
-  }
+    }); 
+  } 
+  /* Redireccionar a la siguiente vista */
+  $('.next').click(function() {
+    event.preventDefault(); 
+    window.location.href = '../views/final-check.html';
+  });
 });
